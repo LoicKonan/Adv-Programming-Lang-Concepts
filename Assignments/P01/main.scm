2c60cdac34097b5fce430a437e24d7dc60326085
Loic <konanloic10@gmail.com>
2022-09-13 23:39:07 -0500

    README.md

---
+++ Assignments/P01/main.scm
@@ -0,0 +1,40 @@
+; Tina Johnson
+; CMPS 5113
+; This program will be passed a positive integer, n, and
+; will return n! One function will use recursion and 
+; a second function will use tail recursion.
+
+; The fac function uses recursion to return n!
+(define (fac n)
+  ; If n = 1, the base case is reached, return 1
+  (if (= n 1)
+      1
+      ; Implied else for non-base case
+      ; Return n * fac(n - 1) 
+      (* n (fac (- n 1)))))
+
+; The facTail function uses tail recursion to return n!
+(define (facTail n tot)
+  ; If n = 1, the base case is reached, return n!
+  ; which is stored in tot.  Using tail recursion
+  ; tot keeps an accumulated value in each call
+  ; to the tail recursive function.
+  (if (= n 1)
+      tot
+      ; Implied else for non-base case
+      ; Return facTail(n-1, n*tot)
+      (facTail (- n 1)(* n tot))))
+
+
+(print "Tina Johnson")
+(print "CMPS 5113")
+(print "This program will print n!  Enter n: ")
+(define n (read))
+(display n)
+(display "! = ")
+(display (fac n))
+(print " using recursion.")
+(display n)
+(display "! = ")
+(display (facTail n 1))
+(print " using tail recursion.")
