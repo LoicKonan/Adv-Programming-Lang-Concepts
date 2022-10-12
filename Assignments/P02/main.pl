1e68a2954c8368a0ebe52fbab0eebb4c87a9d9b0
Loic <konanloic10@gmail.com>
2022-10-12 00:45:06 -0500

    README.md

--- Assignments/P02/main.pl
+++ Assignments/P02/main.pl
@@ -12,52 +12,14 @@
 
 % Make up at least 2 more relationship rules of your own.
 
-male(fowzy).
 student(loic).
-student(kerri).
-student(kevin).
-money(bitcoin).
-money(dollar).
-money(euro).
-money(pound).
-bitcoin(currency).
-ethereum(cryptocurrency).
+male(loic).
+
+student(byron).
+male(byron).
+
+student(fowzy).
+male(fowzy).
 
 
 
-parent(fowzy, kerri).
-parent(fowzy, loic).
-parent(fowzy, josh).
-parent(fowzy, jessica).
-parent(fowzy, jordan).
-parent(fowzy, jacob).
-parent(fowzy, joshua).
-parent(fowzy, jayden).
-parent(fowzy, jay).
-parent(fowzy, jayla).
-parent(fowzy, jaylin).
-
-
-child(kevin, ashley).
-grandparent(jaycee, jayden).
-brother(jayden, jaylin).
-sister(jayden, jaylin).
-nephew(jayden, jaylin).
-niece(jayden, jaylin).
-cousin(jayden, jaylin).
-uncle(jayden, jaylin).
-aunt(jayden, jaylin).
-
-
-% Write a query to find all the children of fowzy.
-(child(X, fowzy)).
-% Write a query to find all the grandchildren of fowzy.
-(grandparent(X, fowzy)).
-% Write a query to find all the brothers of fowzy.
-(brother(X, fowzy)).
-% Write a query to find all the sisters of fowzy.
-(sister(X, fowzy)).
-% Write a query to find all the parents of fowzy.
-(parent(X, fowzy)).
-% Write a query to find all the grandparents of fowzy.
-(grandparent(X, fowzy)).
